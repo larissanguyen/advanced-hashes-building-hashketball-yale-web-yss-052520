d1d4e2d34b48b08433fe7356c45a12b42a2ba937
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-25 10:01:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -83,7 +83,7 @@ def game_hash
         },
         { :player_name = "DeSagna Diop",
           :number = "2",
-          :shoe = "8",
+          :shoe = "14",
           :points = 10,
           :rebounds = 1,
           :assists = 1,
