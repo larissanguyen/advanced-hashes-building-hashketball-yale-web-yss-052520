0de303451e2db1c1f0d5ea8ae05a4e04227e8426
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-25 10:10:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -93,7 +93,7 @@ def game_hash
         },
         { :player_name => "Ben Gordon",
           :number => 8,
-          :shoe => "15",
+          :shoe => 15,
           :points => 33,
           :rebounds => 3,
           :assists => 2,
