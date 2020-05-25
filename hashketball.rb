017fb741a9cc912dd633134ebd447583b65edf80
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-25 09:59:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -59,7 +59,7 @@ def game_hash
     },
     :away => {
       :team_name = "Charlotte Hornets",
-      :colors = ,
+      :colors = ["Turquoise", "Purple"],
       :players = [
         { :player_name = ,
           :number = ,
