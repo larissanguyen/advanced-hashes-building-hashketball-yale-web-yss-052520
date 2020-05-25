a84f5a5fb545d6bd7834ae03c589422c3e3c587e
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-25 09:54:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,7 +6,7 @@ def game_hash
       :colors = ["Black", "White"],
       :players = [
         { :player_name = "Alan Anderson",
-          :number = ,
+          :number = "0",
           :shoe = ,
           :points = ,
           :rebounds = ,
