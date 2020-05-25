7179d0d45a24528b19172fa84accca277bcd627b
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-25 10:02:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -101,7 +101,7 @@ def game_hash
           :blocks = 1,
           :slam_dunks = 0
         },
-        { :player_name = "Jeff Adrien",
+        { :player_name = "Kemba Walker",
           :number = "4",
           :shoe = "8",
           :points = 10,
