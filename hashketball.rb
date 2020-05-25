df4a727b5d9955bb376469d838d5970b9cfd5061
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-25 09:57:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -45,6 +45,16 @@ def game_hash
           :blocks = 8,
           :slam_dunks = 5
         },
+        { :player_name = "Mason Plumlee",
+          :number = "1",
+          :shoe = "19",
+          :points = 26,
+          :rebounds = 11,
+          :assists = 6,
+          :steals = 3,
+          :blocks = 8,
+          :slam_dunks = 5
+        },
         ]
     },
     :away => {
