81c5e22503b9bc88e91ec7090a76f84a0109f3d2
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-25 10:02:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -88,7 +88,7 @@ def game_hash
           :rebounds = 12,
           :assists = 12,
           :steals = 4,
-          :blocks = 7,
+          :blocks = 5,
           :slam_dunks = 2
         },
         { :player_name = "Jeff Adrien",
