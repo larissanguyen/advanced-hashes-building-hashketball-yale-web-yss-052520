10e81042de3043ba9cbd44626fe6333187bb558e
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-25 09:54:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -21,6 +21,16 @@ def game_hash
       :team_name = ,
       :colors = ,
       :players = [
+        { :player_name = ,
+          :number = ,
+          :shoe = ,
+          :points = ,
+          :rebounds = ,
+          :assists = ,
+          :steals = ,
+          :blocks = ,
+          :slam_dunks
+        },
         ]
     }
   }
